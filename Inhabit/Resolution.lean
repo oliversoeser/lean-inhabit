abed4a1e34da3b02a5a436ac5aa54ef8249cc6b8
Oliver Soeser <o.soeser@icloud.com>
2026-06-21 16:41:13 +0100

    substitution

--- Inhabit/Resolution.lean
+++ Inhabit/Resolution.lean
@@ -20,9 +20,9 @@ def GenClause := List Formula
 def StdClause := List Literal
 
 -- Semantics
-abbrev Interpret := Atom → Bool
+def Interp := Atom → Bool
 
-def Formula.eval (i : Interpret) (f : Formula) : Bool :=
+def Formula.eval (i : Interp) (f : Formula) : Bool :=
   match f with
   | top => true
   | btm => false
@@ -34,13 +34,33 @@ def Formula.eval (i : Interpret) (f : Formula) : Bool :=
   | xor l r => l.eval i != r.eval i
   | iff l r => l.eval i == r.eval i
 
-def Literal.eval (i : Interpret) (lit : Literal) : Bool :=
+def Literal.eval (i : Interp) (lit : Literal) : Bool :=
   match lit with
-  | .pos a => i a
-  | .neg a => not (i a)
+  | pos a => i a
+  | neg a => not (i a)
 
-def GenClause.eval (i : Interpret) (gc : GenClause) : Bool :=
+def GenClause.eval (i : Interp) (gc : GenClause) : Bool :=
   (gc.map (Formula.eval i)).foldl .or false
 
-def StdClause.eval (i : Interpret) (sc : StdClause) : Bool :=
+def StdClause.eval (i : Interp) (sc : StdClause) : Bool :=
   (sc.map (Literal.eval i)).foldl .or false
+
+-- Substitution
+def Subst := Atom → Formula
+
+def Formula.subst (σ : Subst) (f : Formula) :=
+  match f with
+  | top => top
+  | btm => btm
+  | atom a => σ a
+  | not f => not (f.subst σ)
+  | or l r => or (l.subst σ) (r.subst σ)
+  | and l r => and (l.subst σ) (r.subst σ)
+  | imp l r => imp (l.subst σ) (r.subst σ)
+  | xor l r => xor (l.subst σ) (r.subst σ)
+  | iff l r => iff (l.subst σ) (r.subst σ)
+
+def Literal.subst (σ : Subst) (lit : Literal) :=
+  match lit with
+  | pos a => σ a
+  | neg a => .not (σ a)
